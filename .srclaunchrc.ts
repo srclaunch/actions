266a434c0fccadeaf3f94e798336aa4c63167c2c
Steven Bennett <steven@budgetbloom.com>
2022-05-11 02:01:12 -0700

    chore: Clean installation cache

--- .srclaunchrc.ts
+++ .srclaunchrc.ts
@@ -12,39 +12,17 @@ import {
   StaticTypingTool,
   TestReporter,
   TestTool,
-} from '@srclaunch/types';
+} from "@srclaunch/types";
 
-const config: Project = {
-  name: '@srclaunch/actions',
-  description: 'SrcLaunch Actions',
+export default <Project>{
+  name: "@srclaunch/actions",
+  description: "SrcLaunch Actions",
   type: ProjectType.Library,
-  build: {
-    formats: [BuildFormat.ESM, BuildFormat.UMD],
-    platform: BuildPlatform.Node,
-    target: BuildTarget.ESNext,
-    tool: BuildTool.Vite,
-  },
   environments: {
     development: {
       formatters: [CodeFormatterTool.Prettier],
       linters: [CodeLinterTool.ESLint],
       staticTyping: [StaticTypingTool.TypeScript],
-    },
-  },
-  release: {
-    publish: {
-      access: PublishAccess.Public,
-      license: License.MIT,
-      registry: 'https://registry.npmjs.org/',
-    },
-  },
-  requirements: {
-    node: '>=16',
-    yarn: '>=3.2.0',
-    srclaunch: {
-      dx: true,
-      cli: true,
-      types: true,
     },
   },
   test: {
@@ -53,6 +31,26 @@ const config: Project = {
     },
     tool: TestTool.Ava,
   },
+  build: {
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Node,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+  release: {
+    publish: {
+      access: PublishAccess.Public,
+      license: License.MIT,
+      registry: "https://registry.npmjs.org/",
+    },
+  },
+  requirements: {
+    node: ">=16",
+    yarn: ">=3.2.0",
+    srclaunch: {
+      dx: true,
+      cli: true,
+      types: true,
+    },
+  },
 };
-
-export default config;
